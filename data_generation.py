88add3b86da16c104a5e4cc62c81a30bab27bcc9
JE <nl21501@bristol.ac.uk>
2022-12-02 16:01:38 +0000

    updated data gen

--- data_generation.py
+++ data_generation.py
@@ -32,34 +32,49 @@ def get_data(random_state=random_seed):
     return train_data, test_data
 
 
-def unbalance(data,class_proportion=0.5):
+def unbalance(data,class_proportions=None, verbose=False):
     '''
     Transfrom balanced dataset into unbalanced dataset
         - data: dictionary with keys 'X', 'y' (must be balanced? - would need to implement an assertion)
     
-        - class_proportion: balance of the classes (default:0.5 is 50/50 split)
-                            classes are unbalanced via undersampling (random sampling without replacement)
-                            Assuming binary classification: class_proportion applies to class 1, with class_proportion - 1 applied to class 2
+        - class_proportions: Decimal value indicating proportional representation of each class.  
+                             Default: Full representation [1,1,...] i.e. [100% of class 1, 100% of class 2,...]
+                             Classes are unbalanced via undersampling (random sampling without replacement)
 
     returns:
         - data: dictionary with keys 'X', 'y'
     '''
 
-    
-    # For each class:
-    #   count class size
+    print('\n rebalancing classes... \n' if verbose else None)
+    # If class proportions left blank, 100% of each class included
+    if class_proportions == None:           
+        class_proportions=[1.0]*len(np.unique(data['y']))
+        print("unbalance warning: No class proportions provided. This is a very expensive way to copy and paste your original dataset." if verbose else None)
+
+
+    labels = np.unique(data['y'][:])   # List of unique class labels
+    unbalanced_i = []                  # List for appending sampling indices
+
+     # For each class:
+    #   Return index of every class instance
+    #   Count class size
     #   shuffle data and take n samples where n = class proporition * class size
-    labels = np.unique(data['y'][:])
-    print(labels)
-    unbalanced_i = []
-    for label in labels:
-        print('N: ',label)
+    
+    for  l in range(0,len(labels)):
+        label = labels[l]
+        proportion = class_proportions[l]  # Moving beyond non-binary really should make this a dictionary 
+        
         label_i = [i for i, x in enumerate(data['y']) if x== label]
         class_size = len(label_i)
-        unbalanced_class_size = int(class_size*abs(label - class_proportion))
-        print('Class ',label,' | Balanced = ',class_size,' , Unbalanced = ',unbalanced_class_size)
-        random.seed(random_seed+label)
+        unbalanced_class_size = int(class_size*proportion)
+
+        random.seed(int(random_seed+label))
         unbalanced_i = [int(i) for i in np.append(unbalanced_i,random.sample(label_i,unbalanced_class_size))]
+
+        print('-'*50)
+        print('Class ',label,' | Balanced = ',class_size,' , Unbalanced = ',unbalanced_class_size)
+        
+        
 
 
     random.seed(random_seed-1)
@@ -90,10 +105,10 @@ if __name__ == '__main__':
     cm_bright = ListedColormap(["#FF0000", "#0000FF"])
 
     train_data, test_data = get_data()
-    class_proportion = 0.2
-    unbalanced_train_data = unbalance(train_data,class_proportion)
+    class_proportions = [1,0.5]
+    unbalanced_train_data = unbalance(train_data,class_proportions)
     
-    unbalanced_test_data = unbalance(test_data,class_proportion)
+    unbalanced_test_data = unbalance(test_data)
     plt.subplot(2,1,1)
     plt.scatter(train_data['X'][:, 0], train_data['X'][:, 1], c=train_data['y'], cmap=cm_bright, edgecolors="k")
     plt.subplot(2,1,2)
