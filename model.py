db2d5f3da05bdee42287af115c8cc02b7323f76d
matt clifford <matt.clifford@bristol.ac.uk>
2022-12-06 17:35:27 +0000

    adjust probs of a model

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ def SVM(data, **kwargs):
 
 def SVM_weighted_training(data):
     return SVM(data, class_weight='balanced')
-    
+
 
 # todo make a superclass  of sklearn classifier
 #   and incorportate input_balancer into:
@@ -90,7 +90,7 @@ class SVM_balance_proba:
     '''
     train a 'black box' model on dataset but balance the model by changing the
     probabilities output from the model by scaling them with respect to the
-    abundance of that class
+    abundance of data points in that class
     input:
         - data: dictionary with keys 'X', 'y'
 
@@ -99,15 +99,35 @@ class SVM_balance_proba:
     '''
     def __init__(self, data):
         self._train_model(data)
+        self._class_weightings(data)
 
     def _train_model(self, data):
         self.clf = SVM(data)
         self.fit_status_ = self.clf.fit_status_
 
+    def _class_weightings(self, data):
+        '''
+        get the weight of each class proportional to the number of instances
+        '''
+        n_samples = len(data['y'])
+        y = np.array(data['y']).astype(np.int64)
+        n_classes = len(list(np.unique(y)))
+        self.bin_count = np.bincount(y)
+        self.c_weights = n_samples / (n_classes * self.bin_count)
+        self.prob_weights = 1 / self.c_weights
 
+    def predict(self, x):
+        return self.clf.predict(x)
 
-
-
+    def predict_proba(self, x):
+        '''
+        reduce the probability of less representative class
+        #### is this what we want to do?????
+        '''
+        preds = self.clf.predict_proba(x)
+        adjusted = preds * self.bin_count
+        adjust_probs = adjusted / np.sum(adjusted, axis=1)
+        return adjust_probs
 
 
 if __name__ == '__main__':
@@ -117,16 +137,18 @@ if __name__ == '__main__':
 
     # get dataset
     train_data, test_data = data_generation.get_data()
+    train_data = data_generation.unbalance_data(train_data,[1,0.5])
 
     # train model
+    # clf = SVM(train_data)
     clf = SVM(train_data)
+    clf_bal_prob = SVM_balance_proba(train_data)
+    print(clf.predict_proba([[1,2]]), sum(sum(clf.predict_proba([[1,2]]))))
+    print(clf_bal_prob.predict_proba([[1,2]]), sum(sum(clf_bal_prob.predict_proba([[1,2]]))))
 
-    # push clf boundairy away from the minority class
-    clf_bal_inp = input_balancer(train_data)
 
-    # plot results
-    ax = plt.gca()
-    train_data['X'] = clf_bal_inp(train_data['X'], weight=1)
-    plot_utils.plot_decision_boundary(clf, train_data, ax=ax)
-    plot_utils.plot_classes(train_data, ax=ax)
-    plt.show()
+    # # plot results
+    # ax = plt.gca()
+    # plot_utils.plot_decision_boundary(clf, train_data, ax=ax)
+    # plot_utils.plot_classes(train_data, ax=ax)
+    # plt.show()
--- plot_utils.py
+++ plot_utils.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn.inspection import DecisionBoundaryDisplay
+import numpy as np
 # plot colours
-cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+cm_bright = ListedColormap(["#0000FF", "#FF0000"])
 cm = plt.cm.RdBu
 
 def plot_classes(data, ax=None):
@@ -16,6 +17,17 @@ def plot_classes(data, ax=None):
     if show == True:
         plt.show()
 
+def plot_decision_boundary_sklearn(clf, data, ax=None):
+    '''
+    plot a decision boundary on axes using sklearn built in method 'DecisionBoundaryDisplay'
+    input:
+        - clf: sklearn classifier object
+    '''
+    ax, show = _get_axes(ax)
+    DecisionBoundaryDisplay.from_estimator(clf, data['X'], cmap=cm, alpha=0.8, ax=ax, eps=0.5)
+    if show == True:
+        plt.show()
+
 def plot_decision_boundary(clf, data, ax=None):
     '''
     plot a decision boundary on axes
@@ -23,11 +35,37 @@ def plot_decision_boundary(clf, data, ax=None):
         - clf: sklearn classifier object
     '''
     ax, show = _get_axes(ax)
-    # ax.scatter(train_data['X'][:, 0], train_data['X'][:, 1], c=train_data['y'], cmap=cm_bright, edgecolors="k")
-    DecisionBoundaryDisplay.from_estimator(clf, data['X'], cmap=cm, alpha=0.8, ax=ax, eps=0.5)
+    # get X from data
+    X = data['X']
+    # define bounds of the domain
+    min1, max1 = X[:, 0].min()-1, X[:, 0].max()+1
+    min2, max2 = X[:, 1].min()-1, X[:, 1].max()+1
+    # define the x and y scale
+    x1grid = np.arange(min1, max1, 0.1)
+    x2grid = np.arange(min2, max2, 0.1)
+    # create all of the lines and rows of the grid
+    xx, yy = np.meshgrid(x1grid, x2grid)
+    # flatten each grid to a vector
+    r1, r2 = xx.flatten(), yy.flatten()
+    r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+    # horizontal stack vectors to create x1,x2 input for the model
+    grid = np.hstack((r1,r2))
+    # make predictions for the grid
+    yhat = clf.predict_proba(grid)
+    # keep just the probabilities for class 0
+    yhat = yhat[:, 0]
+    # reshape the predictions back into a grid
+    zz = yhat.reshape(xx.shape)
+    # plot the grid of x, y and z values as a surface
+    c = ax.contourf(xx, yy, zz, cmap=cm, alpha=0.8)
+    # add a legend, called a color bar
+    plt.colorbar(c)
     if show == True:
         plt.show()
 
+'''
+helper functions
+'''
 def _get_axes(ax):
     '''
     determine whether to make an axes or not, making axes also means show them
@@ -45,8 +83,19 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from sklearn.inspection import DecisionBoundaryDisplay
     import data_generation
+    import model
 
-
+    # ax = plt.gca()
+    _, [ax1, ax2] = plt.subplots(2)
     # get dataset
     train_data, test_data = data_generation.get_data()
-    plot_classes(train_data)
+    train_data = data_generation.unbalance_data(train_data,[1,0.5])
+    clf = model.SVM(train_data)
+    plot_decision_boundary(clf, train_data, ax=ax1)
+    plot_classes(train_data, ax=ax1)
+
+    clf_bal = model.SVM_balance_boundary(train_data)
+    plot_decision_boundary(clf_bal, train_data, ax=ax2)
+    plot_classes(train_data, ax=ax2)
+
+    plt.show()
