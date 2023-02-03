9da8163b2ae7b43ae33c09ff6fb060206f425b8c
matt clifford <matt.clifford@bristol.ac.uk>
2023-02-03 13:26:32 +0000

    init costcla dataloaders

---
+++ clime/data/costcla.py
@@ -0,0 +1,12 @@
+'''
+Get datasets from the costcla package
+    - credit scoring and direct marketing
+'''
+# author: Matt Clifford
+# email: matt.clifford@bristol.ac.uk
+import pandas as pd
+
+
+
+if __name__ == '__main__':
+    main()
