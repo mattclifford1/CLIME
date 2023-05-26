8a22214225caef6a29937ad23c640c9e8bdc50b1
Matt Clifford <matt.clifford@bristol.ac.uk>
2023-05-26 17:31:07 +0100

    start at n dim for contour prob plot

--- clime/utils/plots.py
+++ clime/utils/plots.py
@@ -63,27 +63,35 @@ def plot_decision_boundary(clf, data, ax=None, pca=None):
     ax, show = _get_axes(ax)
     # get X from data
     X = data['X']
+    n_features = X.shape[1]
     # define bounds of the domain
-    min1, max1 = X[:, 0].min()-1, X[:, 0].max()+1
-    min2, max2 = X[:, 1].min()-1, X[:, 1].max()+1
+    lims = []
+    for f in range(n_features):
+        min = X[:, f].min()-1
+        max = X[:, f].max()+1
+        lims.append((min, max))
     # define the x and y scale
-    x1grid = np.arange(min1, max1, 0.1)
-    x2grid = np.arange(min2, max2, 0.1)
+    res = 25
+    xranges = []
+    for f in range(n_features):
+        step = (lims[f][1] - lims[f][0])/ res
+        xranges.append(np.arange(lims[f][0], lims[f][1], step))
     # create all of the lines and rows of the grid
-    xx, yy = np.meshgrid(x1grid, x2grid)
+    xgrids = np.meshgrid(*xranges)
     # flatten each grid to a vector
-    r1, r2 = xx.flatten(), yy.flatten()
-    r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+    flats = []
+    for f in range(n_features):
+        flats.append(xgrids[f].flatten().reshape(-1, 1))
     # horizontal stack vectors to create x1,x2 input for the model
-    grid = np.hstack((r1,r2))
+    grid = np.hstack(flats)
     # make predictions for the grid
     yhat = clf.predict_proba(grid)
     # keep just the probabilities for class 0
     yhat = yhat[:, 0]
     # reshape the predictions back into a grid
-    zz = yhat.reshape(xx.shape)
+    zz = yhat.reshape(xgrids[0].shape)
     # plot the grid of x, y and z values as a surface
-    c = ax.contourf(xx, yy, zz, cmap=cm, alpha=0.8)
+    c = ax.contourf(xgrids[0], xgrids[1], zz, cmap=cm, alpha=0.8)
     # add a legend, called a color bar
     plt.colorbar(c)
     if show == True:
